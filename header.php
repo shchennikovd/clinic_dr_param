30e78500a57a002ce4ff9e7a738f516033339082
Dima Shchennikov <shchennikovdp@yandex.ru>
2026-04-28 09:53:01 +0300

    A button for switching to another page has been added

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Клиника Доктора Парамонова</title>
+    <link rel="stylesheet" href="./assets/fonts.css" />
+    <link rel="stylesheet" href="./assets/variables.css" />
+    <link rel="stylesheet" href="./assets/globals.css" />
+    <link rel="stylesheet" href="./assets/style.css" />
+  </head>
+  <body class="page">
+    <header class="header"></header>
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php include 'header.php'; ?>
+
+<main class="main">
+  <section class="section">
+    <ul class="list-buttons">
+    <li class="list-buttons__button list-buttons__therapy">
+      <a href="#" class="button">
+        <img src="./assets/img/icons/shield.svg" alt="" class="list-buttons__icons list-buttons__icon" aria-hidden="true">
+        <div class="container">
+          <h2 class="list-buttons__title">Лечение по ОМС</h2>
+          <p class="list-buttons__text">Услуги по ОМС. Как получить лечение. Для жителей Саратова и других регионов</p>
+        </div>
+        <img src="./assets/img/icons/arrow.svg" alt="" class="list-buttons__icons list-buttons__arrow" aria-hidden="true">
+      </a>
+      </li>
+      <li class="list-buttons__button list-buttons__therapy">
+        <a href="#" class="button">
+          <img src="./assets/img/icons/shield.svg" alt="" class="list-buttons__icons list-buttons__icon" aria-hidden="true">
+          <div class="container">
+            <h2 class="list-buttons__title">Лечение по ОМС</h2>
+            <p class="list-buttons__text">Услуги по ОМС. Как получить лечение. Для жителей Саратова и других регионов</p>
+          </div>
+          <img src="./assets/img/icons/arrow.svg" alt="" class="list-buttons__icons list-buttons__arrow" aria-hidden="true">
+        </a>
+      </li>
+      <li class="list-buttons__button list-buttons__therapy">
+        <a href="#" class="button">
+          <img src="./assets/img/icons/shield.svg" alt="" class="list-buttons__icons list-buttons__icon" aria-hidden="true">
+          <div class="container">
+            <h2 class="list-buttons__title">Лечение по ОМС</h2>
+            <p class="list-buttons__text">Услуги по ОМС. Как получить лечение. Для жителей Саратова и других регионов</p>
+          </div>
+          <img src="./assets/img/icons/arrow.svg" alt="" class="list-buttons__icons list-buttons__arrow" aria-hidden="true">
+        </a>
+      </li>
+    </ul>
+  </section>
+</main>
+
+<?php include 'footer.php'; ?>
